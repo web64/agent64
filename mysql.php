6fa729bbdb7818f77d89fcdd5dbca09e278b7eb7
Olav Hjertaker <olav@eb64.com>
2017-09-16 17:01:07 +0200

    msg

--- mysql.php
+++ mysql.php
@@ -10,5 +10,6 @@ if ( !$conn->connect_error )
 {
 	echo "OK!";
 }else{
-	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    echo "MySQL ERROR!";
 }
