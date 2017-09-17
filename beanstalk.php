e4dec2aa3eb364d79fbde62b7597d870531dfa64
Olav Hjertaker <olav@eb64.com>
2017-09-17 14:33:50 +0200

    beanstalk

--- beanstalk.php
+++ beanstalk.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require('config.inc.php');
 /**
     Check Beanstalk Connection 
@@ -8,7 +8,7 @@ $fp = fsockopen(BEANSTALK_HOST, BEANSTALK_PORT, $errCode, $errStr, 1);
 if (!$fp) {
     // $errStr ($errCode)
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-    echo "Beanstalk Connection Error $errStr\n";
+    echo "Beanstalk Connection Error [{$errStr}]\n";
 }
 else {
     fclose($fp);
---
+++ mysql-replication-lag.php
@@ -0,0 +1,22 @@
+<?php
+require('config.inc.php');
+/**
+    Check MySQL connection
+*/
+
+$conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
+
+if ( $conn->connect_error )
+{
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    echo "MySQL ERROR!";
+}
+
+if ($result = $mysqli->query("SHOW SLAVE STATUS")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+
+$mysqli->close();
