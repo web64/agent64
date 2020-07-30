9b33b5f049e66dba31d5343b89921614f96c4586
Olav <olav@web64.com>
2020-07-30 14:58:32 +0200

    corenlp

--- corenlp.php
+++ corenlp.php
@@ -9,8 +9,7 @@ $data = file_get_contents("http://localhost:9000");
 
 if ( !empty($data) )
 {
-	header('Content-Type: application/json');
-	echo $data;
+	echo "OK";
 }else{
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
     echo "NLP Server Connection ERROR!";
